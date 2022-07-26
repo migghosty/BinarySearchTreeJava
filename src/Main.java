ab84c947b1747d97cf83463f751beedd07d828b2
migghosty <miguel6886@gmail.com>
2022-07-25 22:28:46 -0700

    Changed file name and separated classes for BST and main

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+class BinarySearchTree {
+    private class Node {
+        private int val;
+        private Node left;
+        private Node right;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+    private Node root;
+
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
